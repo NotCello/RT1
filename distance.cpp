c77421dea6453e96584f7dd4d098276eb5596f87
NotCello <marcello12ori@gmail.com>
2024-11-23 14:54:00 +0100

    dfgbf
    dgfbd

---
+++ distance.cpp
@@ -0,0 +1,68 @@
+#include "ros/ros.h"
+#include "turtlesim/Pose.h"
+#include "geometry_msgs/Twist.h"
+#include "std_msgs/Float32.h"
+#include <cmath>
+
+// Variabili globali per salvare le posizioni
+turtlesim::Pose turtle1Pose, turtle2Pose;
+bool turtle1Received = false, turtle2Received = false;
+
+void turtle1PoseCallback(const turtlesim::Pose::ConstPtr &msg) {
+    turtle1Pose = *msg;
+    turtle1Received = true;
+}
+
+void turtle2PoseCallback(const turtlesim::Pose::ConstPtr &msg) {
+    turtle2Pose = *msg;
+    turtle2Received = true;
+}
+
+float calculateDistance() {
+    return std::sqrt(std::pow(turtle1Pose.x - turtle2Pose.x, 2) +
+                     std::pow(turtle1Pose.y - turtle2Pose.y, 2));
+}
+
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "distance_node");
+    ros::NodeHandle nh;
+
+    ros::Subscriber turtle1Sub = nh.subscribe("/turtle1/pose", 10, turtle1PoseCallback);
+    ros::Subscriber turtle2Sub = nh.subscribe("/turtle2/pose", 10, turtle2PoseCallback);
+
+    ros::Publisher distancePub = nh.advertise<std_msgs::Float32>("/turtle_distance", 10);
+    ros::Publisher turtle2Pub = nh.advertise<geometry_msgs::Twist>("/turtle2/cmd_vel", 10);
+
+    const float distanceThreshold = 1.0;
+    ros::Rate rate(10);
+
+    while (ros::ok()) {
+        if (turtle1Received && turtle2Received) {
+            float distance = calculateDistance();
+
+            // Pubblica la distanza
+            std_msgs::Float32 distanceMsg;
+            distanceMsg.data = distance;
+            distancePub.publish(distanceMsg);
+
+            // Ferma turtle2 se troppo vicina
+            if (distance < distanceThreshold) {
+                ROS_WARN("Turtles too close! Stopping turtle2.");
+                geometry_msgs::Twist stopCmd;
+                turtle2Pub.publish(stopCmd);
+            }
+
+            // Ferma turtle2 se ai confini
+            if (turtle2Pose.x < 1.0 || turtle2Pose.x > 10.0 ||
+                turtle2Pose.y < 1.0 || turtle2Pose.y > 10.0) {
+                ROS_WARN("Turtle2 near boundaries! Stopping.");
+                geometry_msgs::Twist stopCmd;
+                turtle2Pub.publish(stopCmd);
+            }
+        }
+
+        rate.sleep();
+    }
+
+    return 0;
+}
